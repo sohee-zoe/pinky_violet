ce39320f7cf89a8baea9f36226177ce8f9e301ed
kyunghwan51 <kyung133851@pinklab.art>
2024-12-12 17:00:25 +0900

    update pinkylib

--- pinky_bringup/pinky_bringup/pinkylib.py
+++ pinky_bringup/pinky_bringup/pinkylib.py
@@ -27,28 +27,36 @@ class Pinky:
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
         
-        #us sensor
-        GPIO.setup(TRIG, GPIO.OUT)
-        GPIO.setup(ECHO, GPIO.IN)
+        try:
+            #us sensor
+            GPIO.setup(TRIG, GPIO.OUT)
+            GPIO.setup(ECHO, GPIO.IN)
 
-        #buzzer
-        GPIO.setup(BUZZER, GPIO.OUT)
+            #buzzer
+            GPIO.setup(BUZZER, GPIO.OUT)
 
-        #ir
-        GPIO.setup(IR_1, GPIO.OUT)
-        GPIO.setup(IR_2, GPIO.OUT)
-        GPIO.setup(IR_3, GPIO.OUT)
+            #ir
+            GPIO.setup(IR_1, GPIO.IN)
+            GPIO.setup(IR_2, GPIO.IN)
+            GPIO.setup(IR_3, GPIO.IN)
 
-        #motor
-        GPIO.setup(AIN1, GPIO.OUT)
-        GPIO.setup(AIN2, GPIO.OUT)
-        GPIO.setup(PWMA, GPIO.OUT)
-        GPIO.setup(BIN1, GPIO.OUT)
-        GPIO.setup(BIN2, GPIO.OUT)
-        GPIO.setup(PWMB, GPIO.OUT)
-        GPIO.setup(STBY, GPIO.OUT)
+            #motor
+            GPIO.setup(AIN1, GPIO.OUT)
+            GPIO.setup(AIN2, GPIO.OUT)
+            GPIO.setup(PWMA, GPIO.OUT)
+            GPIO.setup(BIN1, GPIO.OUT)
+            GPIO.setup(BIN2, GPIO.OUT)
+            GPIO.setup(PWMB, GPIO.OUT)
+            GPIO.setup(STBY, GPIO.OUT)
+
+        except Exception as e:
+            raise RuntimeError("현재 GPIO가 사용중 입니다. 사용 중인 커널을 종료 후 다시 실행해 주세요")
 
         self.min_speed = 20
+        self.ratio = 1.0
+
+    def set_ratio(self, ratio):
+        self.ratio = float(ratio)
 
     def get_dist(self):
         GPIO.output(TRIG, True)
@@ -68,7 +76,11 @@ class Pinky:
         return distance #cm
     
     def buzzer_start(self):
-        self.pwm = GPIO.PWM(BUZZER, 1000)
+        try:
+            self.pwm = GPIO.PWM(BUZZER, 1000)
+        except:
+            pass
+        
         self.pwm.start(0)
     
     def set_buzzer_duty(self, duty):
@@ -77,12 +89,12 @@ class Pinky:
     def set_buzzer_freq(self, freq):
         self.pwm.ChangeFrequency(freq)
 
-    def buzzer(self, cnt=1):
+    def buzzer(self, cnt=1, duration=0.5, duty=50):
         for i in range(cnt):
-            self.pwm.ChangeDutyCycle(50)
-            time.sleep(0.5)
+            self.pwm.ChangeDutyCycle(duty)
+            time.sleep(duration)
             self.pwm.ChangeDutyCycle(0)
-            time.sleep(0.5)
+            time.sleep(duration)
 
     def buzzer_stop(self):
         self.pwm.stop()
@@ -102,8 +114,12 @@ class Pinky:
         GPIO.output(STBY, GPIO.LOW)
 
     def start_motor(self, freq=1000):
-        self.pwm_L = GPIO.PWM(PWMA, freq)
-        self.pwm_R = GPIO.PWM(PWMB, freq)
+        try:
+            self.pwm_L = GPIO.PWM(PWMA, freq)
+            self.pwm_R = GPIO.PWM(PWMB, freq)
+        except:
+            pass
+        
         self.pwm_L.start(0)
         self.pwm_R.start(0)
     
@@ -118,80 +134,80 @@ class Pinky:
         if L < self.min_speed:
             raise ValueError(f"Set the speed higher than min_speed ({self.min_speed})")
         else: 
-            GPIO.output(AIN1, GPIO.LOW)
-            GPIO.output(AIN2, GPIO.HIGH)
+            GPIO.output(AIN1, GPIO.HIGH)
+            GPIO.output(AIN2, GPIO.LOW)
             self.pwm_L.ChangeDutyCycle(L)
 
         if R < self.min_speed:
             raise ValueError(f"Set the speed higher than min_speed ({self.min_speed})")
         else:    
-            GPIO.output(BIN1, GPIO.LOW)
-            GPIO.output(BIN2, GPIO.HIGH)
+            GPIO.output(BIN1, GPIO.HIGH)
+            GPIO.output(BIN2, GPIO.LOW)
             self.pwm_R.ChangeDutyCycle(R)
 
     def move_back(self, L, R):
         if L < self.min_speed:
             raise ValueError(f"Set the speed higher than min_speed ({self.min_speed})")
         else: 
-            GPIO.output(AIN1, GPIO.HIGH)
-            GPIO.output(AIN2, GPIO.LOW)
+            GPIO.output(AIN1, GPIO.LOW)
+            GPIO.output(AIN2, GPIO.HIGH)
             self.pwm_L.ChangeDutyCycle(L)
 
         if R < self.min_speed:
             raise ValueError(f"Set the speed higher than min_speed ({self.min_speed})")
         else:    
-            GPIO.output(BIN1, GPIO.HIGH)
-            GPIO.output(BIN2, GPIO.LOW)
+            GPIO.output(BIN1, GPIO.LOW)
+            GPIO.output(BIN2, GPIO.HIGH)
             self.pwm_R.ChangeDutyCycle(R)
 
     def turn_right(self, L, R):
         if L < self.min_speed:
             raise ValueError(f"Set the speed higher than min_speed ({self.min_speed})")
         else: 
-            GPIO.output(AIN1, GPIO.LOW)
-            GPIO.output(AIN2, GPIO.HIGH)
+            GPIO.output(AIN1, GPIO.HIGH)
+            GPIO.output(AIN2, GPIO.LOW)
             self.pwm_L.ChangeDutyCycle(L)
 
         if R < self.min_speed:
             raise ValueError(f"Set the speed higher than min_speed ({self.min_speed})")
         else:    
-            GPIO.output(BIN1, GPIO.HIGH)
-            GPIO.output(BIN2, GPIO.LOW)
+            GPIO.output(BIN1, GPIO.LOW)
+            GPIO.output(BIN2, GPIO.HIGH)
             self.pwm_R.ChangeDutyCycle(R)
 
     def turn_left(self, L, R):
         if L < self.min_speed:
             raise ValueError(f"Set the speed higher than min_speed ({self.min_speed})")
         else: 
-            GPIO.output(AIN1, GPIO.HIGH)
-            GPIO.output(AIN2, GPIO.LOW)
+            GPIO.output(AIN1, GPIO.LOW)
+            GPIO.output(AIN2, GPIO.HIGH)
             self.pwm_L.ChangeDutyCycle(L)
 
         if R < self.min_speed:
             raise ValueError(f"Set the speed higher than min_speed ({self.min_speed})")
         else:    
-            GPIO.output(BIN1, GPIO.LOW)
-            GPIO.output(BIN2, GPIO.HIGH)
+            GPIO.output(BIN1, GPIO.HIGH)
+            GPIO.output(BIN2, GPIO.LOW)
             self.pwm_R.ChangeDutyCycle(R)
     
     def move(self, L, R):
         if L < 0:
-            GPIO.output(AIN1, GPIO.HIGH)
-            GPIO.output(AIN2, GPIO.LOW)
-        else: 
             GPIO.output(AIN1, GPIO.LOW)
             GPIO.output(AIN2, GPIO.HIGH)
+        else: 
+            GPIO.output(AIN1, GPIO.HIGH)
+            GPIO.output(AIN2, GPIO.LOW)
         
-        self.pwm_L.ChangeDutyCycle(abs(L))
+        self.pwm_L.ChangeDutyCycle(abs(L * self.ratio))
 
         if R < 0:
-            GPIO.output(BIN1, GPIO.HIGH)
-            GPIO.output(BIN2, GPIO.LOW)
-        else:    
             GPIO.output(BIN1, GPIO.LOW)
             GPIO.output(BIN2, GPIO.HIGH)
+        else:    
+            GPIO.output(BIN1, GPIO.HIGH)
+            GPIO.output(BIN2, GPIO.LOW)
         
-        self.pwm_R.ChangeDutyCycle(abs(R))
+        self.pwm_R.ChangeDutyCycle(abs(R * self.ratio))
 
     def stop(self):
         GPIO.output(AIN1, GPIO.LOW)
@@ -202,7 +218,7 @@ class Pinky:
         GPIO.output(BIN2, GPIO.LOW)
         self.pwm_R.ChangeDutyCycle(0)
     
-    def clean(self, pins=None):
+    def clean(self, pins=None): 
         if pins is None:
             GPIO.cleanup()
         else:
