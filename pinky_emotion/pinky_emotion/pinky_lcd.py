2470dfb0d026cfdeb6bdaec74b4e6a27143f42da
kyunghwan51 <kyung133851@pinklab.art>
2025-01-21 22:20:03 +0900

    Update pinky_lcd.py

--- pinky_emotion/pinky_emotion/pinky_lcd.py
+++ pinky_emotion/pinky_emotion/pinky_lcd.py
@@ -179,6 +179,7 @@ class LCD():
         self.bl.ChangeDutyCycle(value)
 
     def img_show(self, img):
+        img = img.rotate(180)
         img = img.resize((self.h, self.w), Image.LANCZOS)
         img_w, img_h = img.size
         
@@ -201,7 +202,7 @@ class LCD():
         GPIO.output(CS_PIN, GPIO.HIGH)
 
     def clear(self):
-        self._set_windows(0, 0, self.w, self.h)
+        self._set_windows(0, 0, self.h, self.w)
         GPIO.output(DC_PIN, GPIO.HIGH)
         GPIO.output(CS_PIN, GPIO.LOW)
         buf = [0xFF]*(self.w*self.h*2)
@@ -210,7 +211,7 @@ class LCD():
         GPIO.output(CS_PIN, GPIO.HIGH)
 
     def fill_black(self):
-        self._set_windows(0, 0, self.w, self.h)
+        self._set_windows(0, 0, self.h, self.w)
         buf = [0x00, 0x00] * (self.w * self.h)
         GPIO.output(DC_PIN, GPIO.HIGH)
         GPIO.output(CS_PIN, GPIO.LOW)
@@ -221,5 +222,4 @@ class LCD():
     def close(self):
         self.spi.close()
     
-        GPIO.cleanup()
-        
+        GPIO.cleanup([CS_PIN, RST_PIN, DC_PIN, BL_PIN])
